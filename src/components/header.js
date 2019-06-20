1426a8e3ef9ff0618bea653d916bec37acb1842c
Jamie Pittak <charrawr456@gmail.com>
2019-06-20 15:05:11 -0700

    react basic state

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React,{ Component } from 'react';
+import '../css/styles.css';
+    
+class Header extends Component {
+
+    state = {
+        title: 'This is what you are typing: ',
+        keywords: ''
+    }
+
+    inputChangeHandler = (event) => {
+        //console.log(event.target.value)
+        this.setState({
+            keywords: event.target.value
+        })
+    }
+
+    render(){
+        console.log(this.state.keywords)
+        return (
+            <header>
+               <div className="logo"> Logo </div>
+               <input 
+                type= 'text'
+                onChange={this.inputChangeHandler}
+                />
+                <div>
+                    {this.state.title}
+                </div>
+                <div>
+                    {this.state.keywords}
+                </div>
+            </header>
+        )
+    }
+   
+}
+
+export default Header;
