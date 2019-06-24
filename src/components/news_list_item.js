71ae50699434e65e1807ea8e62c43c7450fe9336
Jamie Pittak <charrawr456@gmail.com>
2019-06-24 11:16:01 -0700

    ready for eject

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import {css} from 'glamor';
+import styles from '../css/styles.css';
+import classes from '*.module.css';
 
 
 const NewsListItems = ({item}) => {
 
-    let news_item = css({
-        padding: '20px',
-        boxSizing: 'border-box',
-        borderBottom: '1px solid grey',
-        ':hover':{
-            color: 'red'
-        },
-        '@media(max-width:500px)':{
-            color:'blue'
-        }
-    })
-
-    let item_grey = css ({
-        background: 'lightgrey'
-    })
     return(
-        <div {...news_item} {...item_grey}>
+        <div className={classes.new_item}>
         <h3>{item.title}</h3>
         <div>
             {item.feed}
