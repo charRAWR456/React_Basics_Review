ccca62247fc42c23a55063923316e7d6ed59a4f5
Jamie Pittak <charrawr456@gmail.com>
2019-06-24 11:10:14 -0700

    add and use glamor css library

--- src/components/header.js
+++ src/components/header.js
@@ -4,33 +4,22 @@ import '../css/styles.css';
 class Header extends Component {
 
     state = {
-        active: false,
         keywords: ''
     }
 
     inputChangeHandler = (event) => {
 
-        const value = event.target.value === '' ? false : true;
+        const value = event.target.value === '' ? 'active' : 'non-active';
 
         this.setState({
-            active: value,
             keywords: event.target.value
         })
     }
 
     render(){
 
-        // const style = {
-        //     background: 'red'
-        // }
-
-        // if(this.state.keywords !== ''){
-        //     style.background = 'blue'
-        // } else {
-        //     style.background = 'green'
-        // }
         return (
-            <header style ={{background: `${this.state.active ? 'green': 'blue'}`}}>
+            <header>
                <div className="logo"> Logo </div>
                <input 
                 type= 'text'
